641cfb810379cc0ef4e371e9d6415f3cd53d67aa
Paul <paul@br1software.co.uk>
2024-06-25 14:31:19 +0100

    Added info and help listing. Added first row to test from to get it to be the GUI search.

---
+++ info_and_help.md
@@ -0,0 +1,9 @@
+### Python Classes info -
+https://realpython.com/python-classes/
+
+### customtkinter Widget animation -
+https://www.youtube.com/watch?v=cNftEm-Rs6k
+
+### customtkinter Scrolable frame - 
+https://www.youtube.com/watch?v=Envp9yHb2Ho
+
--- main.py
+++ main.py
@@ -29,20 +29,36 @@ class App(customtkinter.CTk):
 
         self.title("Indexer GUI")
         self.geometry("800x600")
-        self.grid_columnconfigure((0, 1), weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure((1), weight=1)
+        self.grid_rowconfigure(0)
 
+        # Search Label
+        self.search_label = customtkinter.CTkLabel(self, text="Enter your search", fg_color="transparent")
+        self.search_label.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        
+        # Search Box
+        self.search_entry = customtkinter.CTkEntry(self, placeholder_text="enter your search term")
+        self.search_entry.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+        
+        # Include emails
+        switch_var = customtkinter.StringVar(value="on")
+        self.inc_emails = customtkinter.CTkSwitch(self, text="Inc. Emails", command=self.switch_event, variable=switch_var, onvalue="on", offvalue="off")
+        self.inc_emails.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
+        
         self.checkbox_frame_1 = MyCheckboxFrame(self, "Values", values=["value 1", "value 2", "value 3"])
-        self.checkbox_frame_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.checkbox_frame_1.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="nsew")
         self.checkbox_frame_2 = MyCheckboxFrame(self, "Options", values=["option 1", "option 2"])
-        self.checkbox_frame_2.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="nsew")
+        self.checkbox_frame_2.grid(row=1, column=1, padx=(0, 10), pady=(10, 0), sticky="nsew")
 
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+        self.button.grid(row=2, column=0, padx=10, pady=10, sticky="sew", columnspan=2)
         
     def button_callback(self):
         print("checkbox_frame_1:", self.checkbox_frame_1.get())
         print("checkbox_frame_2:", self.checkbox_frame_2.get())
+        
+    def switch_event(self):
+        print("Switch event:", self.inc_emails.get())
 
 if __name__ == "__main__":
     app = App()
