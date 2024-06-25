dd8f497f0b1e853c98c8de9c64bc4cb332daa423
Paul <paul@br1software.co.uk>
2024-06-25 15:53:45 +0100

    Added dotenv to main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import customtkinter
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+server_url = os.getenv("SERVER_URL")
 
 class MyCheckboxFrame(customtkinter.CTkFrame):
     def __init__(self, master, title, values):
@@ -40,10 +46,15 @@ class App(customtkinter.CTk):
         self.search_entry = customtkinter.CTkEntry(self, placeholder_text="enter your search term")
         self.search_entry.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
         
-        # Include emails
-        switch_var = customtkinter.StringVar(value="on")
-        self.inc_emails = customtkinter.CTkSwitch(self, text="Inc. Emails", command=self.switch_event, variable=switch_var, onvalue="on", offvalue="off")
-        self.inc_emails.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
+        # Search Button
+        self.search_button = customtkinter.CTkButton(self, text="Search", command=self.search_button_callback)
+        self.search_button.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
+        
+        # Include Emails
+        switch_var = customtkinter.StringVar(value="off")
+        self.inc_emails = customtkinter.CTkSwitch(self, text="Inc. Emails", command=self.switch_event, 
+                                                  variable=switch_var, onvalue="on", offvalue="off")
+        self.inc_emails.grid(row=0, column=3, padx=10, pady=10, sticky="ew")
         
         self.checkbox_frame_1 = MyCheckboxFrame(self, "Values", values=["value 1", "value 2", "value 3"])
         self.checkbox_frame_1.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="nsew")
@@ -53,6 +64,9 @@ class App(customtkinter.CTk):
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
         self.button.grid(row=2, column=0, padx=10, pady=10, sticky="sew", columnspan=2)
         
+    def search_button_callback(self):
+        print("Search:", self.search_entry.get())
+    
     def button_callback(self):
         print("checkbox_frame_1:", self.checkbox_frame_1.get())
         print("checkbox_frame_2:", self.checkbox_frame_2.get())
